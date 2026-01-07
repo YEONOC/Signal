c00532ff6dcf9eef20ce8e648942e93855d81efe
YEONOC <chlqhtjr752@gmail.com>
2026-01-07 23:53:14 +0900

    chore: delete unused path

--- Source/SIGNAL/SIGNAL.Build.cs
+++ Source/SIGNAL/SIGNAL.Build.cs
@@ -32,19 +32,6 @@ public class SIGNAL : ModuleRules
 
 		PublicIncludePaths.AddRange(new string[] {
 			"SIGNAL",
-			"SIGNAL/Variant_Platforming",
-			"SIGNAL/Variant_Platforming/Animation",
-			"SIGNAL/Variant_Combat",
-			"SIGNAL/Variant_Combat/AI",
-			"SIGNAL/Variant_Combat/Animation",
-			"SIGNAL/Variant_Combat/Gameplay",
-			"SIGNAL/Variant_Combat/Interfaces",
-			"SIGNAL/Variant_Combat/UI",
-			"SIGNAL/Variant_SideScrolling",
-			"SIGNAL/Variant_SideScrolling/AI",
-			"SIGNAL/Variant_SideScrolling/Gameplay",
-			"SIGNAL/Variant_SideScrolling/Interfaces",
-			"SIGNAL/Variant_SideScrolling/UI"
 		});
 
 		// Uncomment if you are using Slate UI
